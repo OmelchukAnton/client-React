e575ccb0ef411f3b8cf32170c502bb9ba9f7dd76
Anton <antony.it.work@gmail.com>
2019-07-06 12:32:39 +0300

    set up env for React

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+var path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+	mode: 'development',
+  	entry: './src/index.js',
+  	output: {
+		path: path.resolve(__dirname, 'build'),
+		publicPath: '/',
+		filename: 'bundle.js'
+  	},
+  	devServer: {
+    	contentBase: "./build",
+  	},
+
+  	module: {
+  		rules: [
+		  	{
+		  		test: /\.(js|jsx)$/,
+		  		exclude: /(node_modules)/,
+		  		use: ['babel-loader', 'eslint-loader']
+		  	},
+		  	{
+		  		test: /\.css$/,
+		        use: ["style-loader", "css-loader"]
+		  	}
+  		]
+  	},
+
+	plugins: [
+	    new HtmlWebpackPlugin({
+			template: path.resolve("./src/index.html"),
+		}),
+  	]
+};
